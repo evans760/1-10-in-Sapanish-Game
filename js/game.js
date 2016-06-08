e6cc71582addf43d31421c3ea03cdbcdd21445eb
Thomas <evans_760@yahoo.com>
2016-06-08 14:53:03 -0700

    day 3.

--- js/game.js
+++ js/game.js
@@ -4,53 +4,78 @@ $(document).ready(function(){
 
 
 
-var score = document.createElement("score")
-var counter=0
+// var score = document.createElement("score")
+var counter;
+
+var words = [];
+
+var random;
+
+reset();
+
+function reset (){
+  words =
+    [{image:"img/uno.jpg", word:"uno", englishNumb:1},
+    {image:"img/dos.jpg", word:"dos", englishNumb:2},
+    {image:"img/tres.jpg", word:"tres", englishNumb:3},
+    {image:"img/cuatro.jpg", word:"cuatro", englishNumb:4},
+    {image:"img/cinco.jpg", word:"cinco", englishNumb:5},
+    {image:"img/seis.jpg", word:"seis", englishNumb:6},
+    {image:"img/siete.jpg", word:"siete", englishNumb:7},
+    {image:"img/ocho.jpg", word:"ocho", englishNumb:8},
+    {image:"img/nueve.jpg", word:"nueve", englishNumb:9},
+    {image:"img/diez.jpg", word:"diez", englishNumb:10}];
+  counter=0;
+  $("#player1").text(counter);
+  gameLogic();
+
+}
 
 
-var random = Math.floor(Math.random()*10)
 
+function gameLogic (){
+  random = Math.floor(Math.random()*words.length);
+  $("#word-image").attr("src", words[random].image);
+}
 
-var words =
-[{image:"img/uno.jpg", word:"uno"},
-{image:"img/dos.jpg", word:"dos"},
-{image:"img/tres.jpg", word:"tres"},
-{image:"img/cuatro.jpg", word:"cuatro"},
-{image:"img/cinco.jpg", word:"cinco"},
-{image:"img/seis.jpg", word:"seis"},
-{image:"img/siete.jpg", word:"siete"},
-{image:"img/ocho.jpg", word:"ocho"},
-{image:"img/nueve.jpg", word:"nueve"},
-{image:"img/diez.jpg", word:"diez"}]
-
-
-
-
-$("#word-image").attr("src", words[random].image)
+$("#resetbtn").click(function(){
+  reset();
+});
 
 $( ".number" ).click(function() {
 
 
-  var englishNumber= parseInt($(this).text())-1
-
-
-
+  var englishNumber= parseInt($(this).text());
 
   var spanish = words[random].word;
+
+
 
   /*var player1 = parseInt(document.getElementById("score").innerHTML);
     player1++;
     */
 
-  if
-    (spanish == words[englishNumber].word) {
+  if (englishNumber == words[random].englishNumb) {
     counter=counter+1;
-    alert("correct" + counter + '/10');
+    $("#player1").text(counter);
+    //run function called gameLogic
+    words.splice(random, 1)
+
+    if (words.length <= 0){
+      swal({ title: "BUENO!/GOOD!",
+        text: "Ahora se puede contar hasta 10 en español! You can now count to 10 in spanish!",
+        imageUrl: "img/count.jpg" });
+      reset();
+    }
+    gameLogic();
 
   }
 
   else{
-    alert("try again");
+
+    swal({ title: "Try agian.",
+      text: "uno, dos, tres, cuatro, cinco, seis, siete, ocho, nueve, diez",
+      imageUrl: "img/count.jpg"});
   }
 
 
